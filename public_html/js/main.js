8f5c519ce0f454a51a84aa7b967284ac08f0f693
Maliuk <oleynikov.dima@gmail.com>
2015-03-04 18:03:51 +0200

    Header without mobile version

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function () {
-    
+
+    //Main Header
+    $('h1.main-title').each(function () {
+        var $p = $(this);
+        $p.html($p.html().replace(/^(\w+)/, '<span>$1</span><br />'));
+    });
+
 });
