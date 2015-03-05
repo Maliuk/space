ca72399f519aa5a721166437f53052d31f04eb43
Maliuk <oleynikov.dima@gmail.com>
2015-03-05 14:03:53 +0200

    Fix main menu. Add bg.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,9 +1,27 @@
 jQuery(document).ready(function () {
-
+    
+    $.pixlayout({
+        src: "page1.png",
+        opacity: 0.8,
+        top: 0,
+        center: true,
+        clip: false,
+        show: false,
+        fixed: true
+    }, ".wrapper");
+    
     //Main Header
     $('h1.main-title').each(function () {
         var $p = $(this);
         $p.html($p.html().replace(/^(\w+)/, '<span>$1</span><br />'));
+    });
+    
+    
+    $('#hide-info').click(function () {
+        if ($('#content').is(':visible'))
+            $('#content').fadeOut(400);
+        else
+            $('#content').fadeIn(400);
     });
 
 });
